f99b4b40ad7e822fe21f3eaa98a2e75e57d5da12
Baron Smile <desigantoshka@gmail.com>
2021-08-25 20:44:35 +0600

    first commit

---
+++ pages/users/[id].js
@@ -0,0 +1,23 @@
+import {useRouter} from "next/router";
+import css from '../../styles/user.module.scss';
+import MainContainer from "../../components/MainContainer";
+
+export default function ({user}) {
+  const {query} = useRouter()
+  return (
+    <MainContainer keywords={user.name}>
+      <div className={css.user}>
+        <h1>Пользователь с id: {query.id}</h1>
+        <div>User name: {user.name}</div>
+      </div>
+    </MainContainer>
+  )
+};
+
+export async function getServerSideProps({params}) {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
+  const user = await response.json();
+  return {
+    props: {user}, // will be passed to the page component as props
+  }
+}
